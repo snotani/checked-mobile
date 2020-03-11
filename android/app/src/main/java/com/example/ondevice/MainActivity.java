3a37943843352f941fe25ea3c050c8fb76796edc
Siddharth Notani <s.notani@lancaster.ac.uk>
2020-03-11 17:39:44 +0000

    Add new android folder

--- android/app/src/main/java/com/example/ondevice/MainActivity.java
+++ android/app/src/main/java/com/example/ondevice/MainActivity.java
@@ -1,5 +1,4 @@
-package com.example.checked_mobile_application;
-
+package com.example.ondevice;
 import org.tensorflow.lite.Interpreter;
 import android.os.Bundle;
 import java.util.ArrayList;
@@ -17,6 +16,7 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import android.content.res.AssetFileDescriptor;
 
+
 public class MainActivity extends FlutterActivity {
 private static final String CHANNEL = "ondeviceML";
 protected Interpreter tflite;
@@ -30,7 +30,8 @@ protected Interpreter tflite;
     } catch (Exception e) {
       //TODO: handle exception
     }
-    
+
+   
     new MethodChannel(getFlutterView(), CHANNEL).setMethodCallHandler(
     new MethodCallHandler() {
         @Override
@@ -50,6 +51,7 @@ protected Interpreter tflite;
     });
   }
   
+ 
   String predictData(ArrayList<Integer> input_data)
   {
     float intArray[][] = new float[1][input_data.size()];
@@ -82,4 +84,15 @@ protected Interpreter tflite;
   long declaredLength = fileDescriptor.getDeclaredLength();
   return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
   }
+
+
+  
 }
+ 
+  
+  
+
+
+
+
+
